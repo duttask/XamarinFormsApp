62699d2b662f331de62facbacf541a88fdc2d467
santoshdutta1517@gmail.com <sanroshdutta1517@gmail.com>
2019-11-18 00:42:56 +0530

    Add project files.

---
+++ XamarinFormsApp/XamarinFormsApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            button.Clicked += Button_Clicked;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string name = entry.Text;
+            DisplayAlert("Hi There", $"Hello {name}", "OK");
+        }
+    }
+}
